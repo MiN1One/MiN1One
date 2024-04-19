92b6f314ba4317b0a1f02e31169133fea2cce4e9
MiN1One <git_min1one@mail.ru>
2024-04-19 05:09:49 +0500

    chore: change personal info

--- src/client/pages/_document.tsx
+++ src/client/pages/_document.tsx
@@ -27,7 +27,7 @@ const Document = () => (
         crossOrigin="anonymous"
       />
       <link
-        href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap"
         rel="stylesheet"
       />
       <link href="/static/fonts/style.css" rel="stylesheet" />
